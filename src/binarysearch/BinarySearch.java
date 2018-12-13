afcab097a3934f9c7750cbc615ab87b37d534d78
tsfapps <appslelo.com@gmail.com>
2018-12-13 17:44:52 +0530

    Binary Search using recursion function in Java

--- src/binarysearch/BinarySearch.java
+++ src/binarysearch/BinarySearch.java
@@ -4,14 +4,14 @@ package binarysearch;
 
 public class BinarySearch {
 
-  int binarySearch(int arr[], int l, int r, int x){
-      if(r >= l){
+  int binarySearch(int arr[], int f, int r, int x){
+      if(r >= f){
           
-      int mid = l + (r-l)/2;
+      int mid = f + (r-f)/2;
       if (arr[mid] == x)
           return mid;
      if (arr[mid] > x)
-          return binarySearch(arr, l, mid-1, x);
+          return binarySearch(arr, f, mid-1, x);
      
           return binarySearch(arr, mid+1, r, x);
       }
@@ -22,7 +22,7 @@ public class BinarySearch {
         BinarySearch bs = new BinarySearch();
         int arr[] = {3, 6, 8, 10, 13, 15, 17, 19, 20, 26, 29, 36, 39};
         int n = arr.length;
-        int x = 15;
+        int x = 29;
         int result = bs.binarySearch(arr, 0, n-1, x);
         if (result == -1){
             System.out.println("Element not found");
